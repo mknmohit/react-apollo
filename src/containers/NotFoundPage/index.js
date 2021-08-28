70af4e4fcfff6c824a324628f02c1d5e3ae19578
Mohit Kumar <mohitkumar.7@outlook.com>
2021-08-28 22:39:55 +0530

    add 404 page not found

---
+++ src/containers/NotFoundPage/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Result, Button } from 'antd';
+import { Link } from "react-router-dom";
+import './style.css';
+
+export default function NotFoundPage() {
+  return (
+    <div className="not-found-root">
+      <div className="not-found-container">
+        <Result
+          status="404"
+          title="404"
+          subTitle="Sorry, the page you visited does not exist."
+          extra={<Link to="/"><Button type="primary">Back Home</Button></Link>}
+        />
+      </div>
+    </div>
+  );
+}
