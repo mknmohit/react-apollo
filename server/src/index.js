625ed13c27456c73d809a1264177ea9a357c1ba3
Mohit Kumar <mohitkumar.7@outlook.com>
2021-08-30 14:37:57 +0530

    clean server files

--- server/src/index.js
+++ server/src/index.js
@@ -1,17 +1,9 @@
-const { ApolloServer, PubSub } = require('apollo-server');
+const { ApolloServer } = require('apollo-server');
 const { PrismaClient } = require('@prisma/client');
 const Query = require('./resolvers/Query');
 const Mutation = require('./resolvers/Mutation');
-// const Subscription = require('./resolvers/Subscription');
-// const User = require('./resolvers/User');
-// const Link = require('./resolvers/Link');
-// const Vote = require('./resolvers/Vote');
-// const Post = require('./resolvers/Post');
 const fs = require('fs');
 const path = require('path');
-// const { getUserId } = require('./utils');
-
-// const pubsub = new PubSub();
 
 const prisma = new PrismaClient({
   errorFormat: 'minimal'
@@ -20,10 +12,6 @@ const prisma = new PrismaClient({
 const resolvers = {
   Query,
   Mutation,
-  // Subscription,
-  // User,
-  // Link,
-  // Vote
   Post: {
     id: parent => parent.id,
     title: parent => parent.title,
@@ -46,30 +34,8 @@ const server = new ApolloServer({
     return {
       ...req,
       prisma,
-      // pubsub,
-      // userId:
-        // req && req.headers.authorization
-        //   ? getUserId(req)
-        //   : null
     };
   },
-  // subscriptions: {
-  //   onConnect: (connectionParams) => {
-  //     if (connectionParams.authToken) {
-  //       return {
-  //         prisma,
-  //         userId: getUserId(
-  //           null,
-  //           connectionParams.authToken
-  //         )
-  //       };
-  //     } else {
-  //       return {
-  //         prisma
-  //       };
-  //     }
-  //   }
-  // }
 });
 
 server
--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -1,56 +1,5 @@
-// async function feed(parent, args, context, info) {
-//   const where = args.filter
-//     ? {
-//         OR: [
-//           { description: { contains: args.filter } },
-//           { url: { contains: args.filter } }
-//         ]
-//       }
-//     : {};
-
-//   const links = await context.prisma.link.findMany({
-//     where,
-//     skip: args.skip,
-//     take: args.take,
-//     orderBy: args.orderBy
-//   });
-
-//   const count = await context.prisma.link.count({ where });
-
-//   return {
-//     id: 'main-feed',
-//     links,
-//     count
-//   };
-// }
-
-// let dummyPosts = [
-//   {
-//     id: "post-0",
-//     title: "sample dummy title 0",
-//     description: "sample dummy description 0",
-//   },
-//   {
-//     id: "post-1",
-//     title: "sample dummy title 1",
-//     description: "sample dummy description 1",
-//   },
-//   {
-//     id: "post-2",
-//     title: "sample dummy title 2",
-//     description: "sample dummy description 2",
-//   },
-// ];
-
 const getAllPosts = async (parent, args, context, info) => {
-  // console.log("info: ", info);
-  // console.log("context: ", context);
-  // console.log("args: ", args);
-  // console.log("parent: ", parent);
-  // console.log("context.prisma: ", context.prisma);
-  console.log("args: ", args);
   const totalPost = context.prisma.post.count();
-  console.log("totalPost: ", totalPost);
   const skip = args.itemsPerPage * (args.currentPage - 1);
   const posts = context.prisma.post.findMany({
     skip,
@@ -69,7 +18,6 @@ const getSinglePost = async (parent, args, context, info) => {
       id: Number(args.id),
     },
   });
-  console.log("singlePost: ", singlePost);
   if (!singlePost) {
     throw new Error(`Post with id: ${args.id} not found!`);
   }
