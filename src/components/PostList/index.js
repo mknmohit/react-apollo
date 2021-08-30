8b5a4cb7028cdfa5e7f726b784ca116caef15b7c
Mohit Kumar <mohitkumar.7@outlook.com>
2021-08-30 14:03:34 +0530

    add pagination ui and it's functionality

--- src/components/PostList/index.js
+++ src/components/PostList/index.js
@@ -1,25 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "@apollo/client";
 import { useHistory } from "react-router";
 import { LINKS_PER_PAGE } from "../../constants";
-import ScrollToTop from '../../components/ScrollToTop';
+import ScrollToTop from "../../components/ScrollToTop";
 import { Link } from "react-router-dom";
-import { Card, Skeleton } from "antd";
+import { Card, Pagination, Skeleton } from "antd";
 import { RightOutlined } from "@ant-design/icons";
 import truncate from "lodash.truncate";
-import { POSTS_PER_PAGE } from "../../utils/constants"
+import { POSTS_PER_PAGE } from "../../utils/constants";
 import { POSTS_QUERY } from "./query";
 import "./styles.css";
 
 const { Meta } = Card;
 
 const PostList = () => {
+  const [currentPageState, setCurrentPageState] = useState(1);
+  const [postsPerPageState, setPostsPerPageState] = useState(POSTS_PER_PAGE);
+
   const { data, loading, error } = useQuery(POSTS_QUERY, {
     variables: {
-      currentPage: 1,
-      itemsPerPage: POSTS_PER_PAGE
-    }
+      currentPage: currentPageState,
+      itemsPerPage: postsPerPageState,
+    },
   });
+
+  const onChangePage = (page) => {
+    setCurrentPageState(page);
+  };
+
+  const onShowSizeChange = (current, size) => {
+    setPostsPerPageState(size);
+  }
+
+  const renderPagination = () => {
+    if (data?.getAllPosts?.total) {
+      return (
+        <div className="pagination">
+          <Pagination
+            defaultCurrent={data.getAllPosts.currentPage}
+            total={data.getAllPosts.total}
+            defaultPageSize={postsPerPageState}
+            onChange={onChangePage}
+            showSizeChanger
+            onShowSizeChange={onShowSizeChange}
+            pageSizeOptions={[3, 6, 10, 20, 50, 100]}
+          />
+        </div>
+      );
+    }
+  };
 
   const renderCard = () => {
     if (loading) {
@@ -29,7 +58,7 @@ const PostList = () => {
           <Skeleton active />
           <Skeleton active />
         </div>
-      )
+      );
     }
     return data?.getAllPosts?.results?.map((item) => {
       return (
@@ -41,7 +70,10 @@ const PostList = () => {
             title={item.title}
             extra={<RightOutlined className="card-icon" />}
           >
-            <Meta className="capitalize" description={truncate(item.description, {length: 400})} />
+            <Meta
+              className="capitalize"
+              description={truncate(item.description, { length: 400 })}
+            />
           </Card>
         </Link>
       );
@@ -50,7 +82,10 @@ const PostList = () => {
 
   return (
     <ScrollToTop>
-      <section className="posts">{renderCard()}</section>
+      <section className="posts">
+        {renderCard()}
+        {renderPagination()}
+      </section>
     </ScrollToTop>
   );
 };
